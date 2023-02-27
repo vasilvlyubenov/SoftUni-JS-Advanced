eacbf9d3b6159d4db0f3a166ad40f38270adbe12
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-02-28 00:11:09 +0200

    more exercises

--- JS Applications/05. Accordion/accordion.js
+++ JS Applications/05. Accordion/accordion.js
@@ -1,3 +1,48 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    const main = document.getElementById('main');
+
+    const articleNames = await fetch('http://localhost:3030/jsonstore/advanced/articles/list').then(name => name.json());
+    
+    for (const article of articleNames) {
+        const id = article._id;
+        const titleName = article.title;
+
+        const content = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${id}`).then(data => data.json());
+
+        const accordionDiv = document.createElement('div');
+        accordionDiv.className = 'accordion';
+        const headDiv = document.createElement('div');
+        headDiv.className = 'head';
+        const span = document.createElement('span');
+        span.textContent = titleName;
+        const button = document.createElement('button');
+        button.className = 'button';
+        button.setAttribute('id', `${id}`);
+        button.textContent = 'More';
+        headDiv.appendChild(span);
+        headDiv.appendChild(button);
+        accordionDiv.appendChild(headDiv);
+        const extraDiv = document.createElement('div');
+        extraDiv.className = 'extra';
+        extraDiv.style.display = 'none';
+        const p = document.createElement('p');
+        p.textContent = content.content;
+        extraDiv.appendChild(p);
+        accordionDiv.appendChild(extraDiv);
+        main.appendChild(accordionDiv);
+
+        button.addEventListener('click', () => {
+            if (extraDiv.style.display === 'none') {
+                extraDiv.style.display = 'block';
+                button.textContent = 'Less';
+            } else {
+                extraDiv.style.display = 'none';
+                button.textContent = 'More';
+            }
+        });
+
+    };
+
 }
+
+solution();
--- JS Applications/06.Blog/app.js
+++ JS Applications/06.Blog/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    console.log('TODO...');
+    const btnLoad = document.getElementById('btnLoadPosts');
+    const btnView = document.getElementById('btnViewPost');
+    const selectMenu = document.getElementById('posts');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const postComments = document.getElementById('post-comments');
+    const bodyObj = {};
+
+    btnLoad.addEventListener('click', loadPosts);
+    btnView.addEventListener('click', loadView);
+
+    async function loadPosts() {
+        selectMenu.replaceChildren();
+        postComments.replaceChildren();
+        const titles = await fetch('http://localhost:3030/jsonstore/blog/posts').then(title => title.json());
+
+        for (const key in titles) {
+            const id = titles[key].id;
+            const title = titles[key].title;
+            const body = titles[key].body;
+
+            const option = document.createElement('option');
+            option.value = id;
+            option.textContent = title;
+            selectMenu.appendChild(option);
+
+            bodyObj[id] = body;
+
+        }
+    }
+
+    async function loadView() {
+        postComments.replaceChildren();
+        const optinValue = document.getElementById('posts');
+        const content = await fetch('http://localhost:3030/jsonstore/blog/comments').then(content => content.json());
+        const comments = Object.values(content).filter(el => el.postId === optinValue.value);
+        postBody.textContent = bodyObj[optinValue.value];
+        postTitle.textContent = optinValue.selectedOptions[0].textContent;
+
+
+        comments.forEach(comment => {
+            const li = document.createElement('li');
+            li.textContent = comment.text;
+            postComments.appendChild(li);
+        });
+
+    }
 }
 
 attachEvents();
