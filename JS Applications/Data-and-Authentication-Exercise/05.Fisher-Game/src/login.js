c24cd48f939981d61147feb524f9632cb4923741
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-03-05 18:11:34 +0200

    fisher-game

--- JS Applications/Data-and-Authentication-Exercise/05.Fisher-Game/src/login.js
+++ JS Applications/Data-and-Authentication-Exercise/05.Fisher-Game/src/login.js
@@ -1,1 +1,44 @@
-console.log('TODO:// Implement Login functionality');
+import { checkLogin } from "./app.js";
+checkLogin();
+
+const loginUrl = 'http://localhost:3030/users/login';
+
+document.getElementById('login-form').addEventListener('submit', login);
+
+async function login(event) {
+    event.preventDefault();
+
+    const loginForm = new FormData(event.target);
+
+    const {email, password} = Object.fromEntries(loginForm.entries());
+
+    if (email === '' || password === '') {
+        return alert('All fields must me filled!');
+    }
+
+    try {
+        const response = await fetch(loginUrl, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+
+        if (!response.ok) {
+            const err = await response.json();
+            throw err;
+        }
+
+        const tokens = await response.json();
+
+        localStorage.setItem('email', tokens.email);
+        localStorage.setItem('id', tokens._id);
+        localStorage.setItem('accessToken', tokens.accessToken);
+        event.target.reset();
+        location = '../index.html';
+    } catch (error) {
+        document.getElementsByClassName('notification')[0].textContent = error.message;
+    }
+}
--- JS Applications/Data-and-Authentication-Exercise/06.Furniture/solution.js
+++ JS Applications/Data-and-Authentication-Exercise/06.Furniture/solution.js
@@ -1,4 +1,32 @@
 function solve() {
+  location = 'home.html';
 
-  //TODO...
+  document.querySelector('tbody');
+
+
+}
+
+function populateProducts() {
+
+}
+
+getProducts();
+
+async function getProducts() {
+  const url = 'http://localhost:3030/data/furniture';
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw await response.json();
+    }
+
+    const data = await response.json();
+
+    return data;
+
+  } catch (error) {
+    alert(error.message);
+  }
 }
